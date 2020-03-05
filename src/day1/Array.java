c69092b5ab7e9e94fc4414dda1da075b26b30afe
sezginhmrc <sezginhamurcu@icloud.com>
2020-03-05 16:20:26 -0500

    array added

---
+++ src/day1/Array.java
@@ -0,0 +1,18 @@
+package day1;
+
+import java.util.Arrays;
+
+public class Array {
+
+    public static void main(String[] args) {
+
+        int [] nums = new int [5];
+
+        nums[1] = 123;
+        nums[0] = 4343;
+        nums[4] =123123;
+        nums[2] = 666;
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
