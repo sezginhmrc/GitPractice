d8e8e286b314909c7acd1483a34b9de9275fbbf1
sezginhmrc <sezginhamurcu@icloud.com>
2020-03-05 16:02:10 -0500

    Reversing String

---
+++ src/day1/ReversinString.java
@@ -0,0 +1,20 @@
+package day1;
+
+public class ReversinString {
+
+    public static void main(String[] args) {
+
+
+        String name = "SEZGINOZLEMHAMURCU";
+
+        String reverse = "";
+
+        // red means files not ready for commit
+        // green means files ready for commit -> staging for commit
+        // if we setup automation staging files will look green
+        // to save .gitignore -> command s.
+
+
+    }
+
+}
